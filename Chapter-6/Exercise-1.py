4d03a7b72b084ac30117ec4be2ceb739e5bdb6ff
Snake8975 <79586620+Snake8975@users.noreply.github.com>
2026-09-01 17:32:12 +0200

    Add files via upload

---
+++ Chapter-6/Exercise-1.py
@@ -0,0 +1,16 @@
+# Recupero la stringa da stampare
+varString = input("Inserisci la stringa da stampare: ")
+varString = varString
+varLenStr = int(len(varString))
+varLenStrNeg = varLenStr * -1
+varIdx = -1
+
+# Test 1 - Stampa caratteri al contrario con while
+print("Test con while")
+while varIdx >= varLenStrNeg:
+    print(varString[varIdx])
+    varIdx -= 1
+
+
+
+
